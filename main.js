3624638285681354b34035f55713829d94f0735a
Marlovy Renteria Cuero <cuerorenteriamarlovy33@gmail.com>
2023-10-20 15:34:52 -0500

    avance 4

--- main.js
+++ main.js
@@ -1,49 +1,21 @@
 import "./style.css";
-//boton para cargar mas peliculas
-let loadMorebtn1 = document.querySelector("#load-more-1");
+// //boton para cargar mas peliculas
+// const BtnAbrirMenu = document.querySelector("");
+// const BtnCerrarMenu = document.querySelector("");
+// const formularioContainer = document.querySelector("");
+// const container = document.querySelector("");
+// const formulario = document.querySelector("");
+// const eliminarPersonaje = document.querySelector("")
 
-let currentItem1 = 4;
+// // Abrir formulario con el boton del header
+// BtnAbrirMenu.addEventListener("click", () => {
+//   formularioContainer.classList.add("abrirFormulario");
+// });
 
-loadMorebtn1.onclick = () => {
-  let boxes = [...document.querySelectorAll(".box-container-1 box-1")];
-  for (var i = currentItem1; i < currentItem1 + 4; i++) {
-    boxes[i].style.display = "inline block";
-  }
-  currentItem1 += 4;
-  if (currentItem1 >= boxes.length) {
-    loadMorebtn1.style.display = "none";
-  }
-};
-
-let loadMorebtn2 = document.querySelector("#load-more-2");
-
-let currentItem2 = 4;
-
-loadMorebtn2.onclick = () => {
-  let boxes = [...document.querySelectorAll(".box-container-2 box-2")];
-  for (var i = currentItem2; i < currentItem2 + 4; i++) {
-    boxes[i].style.display = "inline block";
-  }
-  currentItem2 += 4;
-  if (currentItem2 >= boxes.length) {
-    loadMorebtn2.style.display = "none";
-  }
-};
-
-let loadMorebtn3 = document.querySelector("#load-more-3");
-
-let currentItem3 = 4;
-
-loadMorebtn3.onclick = () => {
-  let boxes = [...document.querySelectorAll(".box-container-3 box-3")];
-  for (var i = currentItem3; i < currentItem3 + 4; i++) {
-    boxes[i].style.display = "inline block";
-  }
-  currentItem3 += 4;
-  if (currentItem3 >= boxes.length) {
-    loadMorebtn3.style.display = "none";
-  }
-};
+// // Cerrar formulario con el boton del formulario
+// BtbCerrarMenu.addEventListener("click", () => {
+//   formularioContainer.classList.remove("abrirFormulario");
+// });
 
 //boton para añadir pelicula, formulario
 
